0b9fa5c861d7f2d6d0876c767ac83615d0271bf9
nirazz <1nirazz@gmail.com>
2020-01-10 22:00:17 +0200

    pushing weekly assignment

---
+++ Weekly Assignment - Personal website/index.js
@@ -0,0 +1,26 @@
+var count = new Date("feb 27,2020 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var d = count - now;
+
+
+    var days = Math.floor(d / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((d % (1000 * 60)) / 1000);
+
+    // var days = Math.floor(d / (750000 * 80 * 60 * 24));
+    // var hours = Math.floor((d % (10000 * 8 * 60 * 24)) / (1000 * 60 * 60));
+    // var minutes = Math.floor((d % (1000 * 10 * 60)) / (1000 * 60));
+    // var seconds = Math.floor((d % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if (d <= 0) {
+        clearInterval(x);
+    }
+
+}, 1000);
